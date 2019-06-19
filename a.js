163886e8a07a4e455f9adecd458d65a6a7818284
Narendra Sisodiya <narendra.sisodiya.1983@gmail.com>
2019-06-19 12:49:04 +0530

    asyncMapPool

--- a.js
+++ a.js
@@ -6,6 +6,36 @@ var getTime = (() => {
 Array.prototype.asyncMapP = function(t) {
   return Promise.all(this.map(t));
 };
+
+Array.prototype.asyncMapPool = async function(t, poolSize) {
+  var fullpool = [];
+  var isCompleted = this.map(() => false);
+  var length = this.length;
+  var self = this;
+
+  return new Promise(function(resolve, reject) {
+    function addNewToPool() {
+      if (i < length) {
+        var copyI = i;
+        fullpool.push(
+          t(self[i], i, self).then(function(v) {
+            isCompleted[copyI] = true;
+            if (isCompleted.filter(v => v === false).length === 0) {
+              resolve(Promise.all(fullpool));
+            }
+            addNewToPool(); //add a new to pool.
+            return v;
+          })
+        );
+      }
+      i++;
+    }
+    for (var i = 0; i < poolSize; ) {
+      addNewToPool();
+    }
+  });
+};
+
 Array.prototype.asyncMapS = async function(t) {
   for (var r = [], a = 0; a < this.length; a++)
     r.push(await t(this[a], a, this));
@@ -18,14 +48,14 @@ Array.prototype.asyncReduce = async function(t, r) {
 };
 
 function getSquare(v, i, arr) {
-  console.log(`executing getSquare at ${getTime()}`);
+  console.log(`executing getSquare at ${getTime()} , ${v}, ${i}`);
   return v * v;
 }
 
 function getSquareAsync(v, i, arr) {
   return new Promise(resolve =>
     setTimeout(() => {
-      console.log(`executing getSquareAsync at ${getTime()}`);
+      console.log(`executing getSquareAsync at ${getTime()}, ${v}, ${i}`);
       resolve(v * v);
     }, 2000)
   );
@@ -58,18 +88,18 @@ async function run() {
   var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   var result1 = arr.map(getSquare);
   console.log(`result1 @ ${getTime()}`, result1);
-  var result2 = await arr.asyncMapP(getSquareAsync);
+  var result2 = await arr.asyncMapPool(getSquareAsync, 1);
 
   console.log(`result2 @ ${getTime()}`, result2);
 
-  var result3 = await arr.asyncMapS(getSquareAsync);
-  console.log(`result3 @ ${getTime()}`, result3);
+  // var result3 = await arr.asyncMapS(getSquareAsync);
+  // console.log(`result3 @ ${getTime()}`, result3);
 
-  var result4 = arr.reduce(getArraySum, 0);
-  console.log(`result4 @ ${getTime()} - Sum is`, result4);
+  // var result4 = arr.reduce(getArraySum, 0);
+  // console.log(`result4 @ ${getTime()} - Sum is`, result4);
 
-  var result5 = await arr.asyncReduce(getArraySumAsync, 0);
-  console.log(`result5 @ ${getTime()} - Sum is`, result5);
+  // var result5 = await arr.asyncReduce(getArraySumAsync, 0);
+  // console.log(`result5 @ ${getTime()} - Sum is`, result5);
 }
 
 run();
